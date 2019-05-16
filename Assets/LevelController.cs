7baf760771cf37570b20db9137280383dfe63281
samlaw2112 <samuel.lawrence2112@gmail.com>
2019-05-16 11:50:49 +0100

    Sets of platforms spawn ahead of player

--- Assets/LevelController.cs
+++ Assets/LevelController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
-/// Positioned and level start (player spawn location). Responsible for keeping track of various
+/// Positioned at level start (player spawn location). Responsible for keeping track of various
 /// game objects and handles level events such as player death and respawn
 /// </summary>
 public class LevelController : MonoBehaviour
@@ -12,12 +12,15 @@ public class LevelController : MonoBehaviour
     private GameObject player;
     private SwipeController swipeController;
     private CameraHandle cameraHandle;
+    private PlatformSpawner platformSpawner;
     private bool firstSpawn = true;
 
     private void Awake()
-    {
+    { 
+        // Asuming only one of each of the following
         swipeController = FindObjectOfType<SwipeController>();
         cameraHandle = FindObjectOfType<CameraHandle>();
+        platformSpawner = FindObjectOfType<PlatformSpawner>();
     }
 
     private void Start()
@@ -30,17 +33,17 @@ public class LevelController : MonoBehaviour
     {
         player = Instantiate(playerPrefab, transform.position, Quaternion.identity) as GameObject;
         player.transform.SetParent(transform);
-        // Only need to reset the gravity for respawns, not on initial spawn
+        // Tell other game objects about new player
         swipeController.SetNewPlayer(GetPlayer());
         swipeController.SetFirstLaunch();
+        cameraHandle.SetPlayer(GetPlayer());
+        platformSpawner.SetPlayer(GetPlayer());
         // Only need to reset the camera for respawns, not on initial spawn
         if (firstSpawn)
         {
-            cameraHandle.SetPlayer(GetPlayer());
             firstSpawn = false;
         } else
         {
-            cameraHandle.SetPlayer(GetPlayer());
             cameraHandle.ResetCamera();
         }
     }
---
+++ Assets/Platforms/PlatformSpawner.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Responsible for spawning platform sets ahead of player.
+/// </summary>
+public class PlatformSpawner : MonoBehaviour
+{
+    public GameObject[] platformSets = new GameObject[1];
+
+    private LevelController levelController;
+    private Player player;
+    // Spawn location for next spawn of set of platforms (in x)
+    private float nextSpawnLocationX = 0f; // initialised to 0
+    private float distanceBetweenSpawns = 660f; // Sensible default assuming all platform sets are the same length
+    private float distanceBetweenCheckpointAndNextSpawn = 300f; // Sensible default
+    // Checkpoint location, next platform set will be spawned when player crosses checkpoint
+    private float checkpointLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Assuming only one level controller
+        levelController = FindObjectOfType<LevelController>();
+
+        // Set up for spawn of next set
+        UpdateNextSpawnLocation();
+        SetNextCheckpointLocation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If player passes checkpoint location
+        if (player.transform.position.x > checkpointLocation)
+        {
+            // Spawn next set of platforms at the right location
+            SpawnNextPlatformSet();
+
+            // Set up for spawn of next set
+            UpdateNextSpawnLocation();
+            SetNextCheckpointLocation();
+        }
+
+    }
+
+    
+    public void SetPlayer(Player newPlayer)
+    {
+        player = newPlayer;
+    }
+
+    void SetNextCheckpointLocation()
+    {
+        checkpointLocation = nextSpawnLocationX - distanceBetweenCheckpointAndNextSpawn;
+    }
+
+    void SpawnNextPlatformSet()
+    {
+        // Choose a random set to spawn
+        // TODO increase spawn rate of more difficult sets based on distance travelled
+        int setToSpawn = Random.Range(0, platformSets.Length);
+        // Spawn at next spawn location
+        Vector3 spawnLocation = new Vector3(nextSpawnLocationX, 0, 0);
+        GameObject platformSet = Instantiate(platformSets[setToSpawn], spawnLocation, Quaternion.identity);
+        platformSet.transform.SetParent(this.transform);
+    }
+
+    void UpdateNextSpawnLocation()
+    {
+        nextSpawnLocationX += distanceBetweenSpawns;
+    }
+}
