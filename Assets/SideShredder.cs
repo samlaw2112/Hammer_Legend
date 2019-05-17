91b1b75f3f1af45bb6fe480cee7a640edce9ae1c
samlaw2112 <samuel.lawrence2112@gmail.com>
2019-05-17 09:35:04 +0100

    Added side shredder that destroy passed platforms

---
+++ Assets/SideShredder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideShredder : MonoBehaviour
+{
+    LevelController levelController;
+
+    private void Start()
+    {
+        levelController = FindObjectOfType<LevelController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Player player = collider.GetComponentInParent<Player>();
+        Platform platform = collider.GetComponent<Platform>();
+        if (player)
+        {
+            levelController.DestroyPlayerAndStartNewGame();
+        }
+        else if (platform)
+        {
+            Destroy(collider.gameObject);
+        }
+
+    }
+
+}
